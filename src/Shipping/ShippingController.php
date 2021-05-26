32d14369ea7cf0bee39a04d0906b83c546ec3c22
Razvan Goldstein <razvan.goldstein@gmail.com>
2021-05-26 14:39:54 +0300

    added methods for getting estimate and data

--- src/Shipping/ShippingController.php
+++ src/Shipping/ShippingController.php
@@ -15,9 +15,9 @@ class ShippingController
     }
 
     /**
-     * @todo: get estimated delivery time 
+     * @todo: add validation for range
      * 
-     * Get estimated delivery time based on a zip code
+     * Get estimated delivery time based on a zip code and optional range
      * @param {string} $zip_code: zip code to get estimate for
      * @param {array} $range: date range for which historical data should be used
      * @return {string} date string with estimated delivery date
@@ -30,10 +30,35 @@ class ShippingController
         if (!$validZip) {
             throw new Exception('Invalid zip code submitted');
         }
-        // @todo - load historical data
-        // @todo - filter historical data based on zip code and range
-        // @todo - get estimated delivery
-        // @todo - return date string estimated delivery time
-        return 'hello';
+        
+        return $this->getEstimate($zip_code, $range);
+    }
+
+    /**
+     * @todo - calculate average based on historical data
+     * 
+     * Get estimated delivery for given zip code and range
+     * @param {string} $zip_code: zip code to get estimate for
+     * @param {array} $range: date range for which historical data should be used
+     * @return {string} date string with estimated delivery date 
+     */
+    private function getEstimate(string $zip_code, array $range): string
+    {
+        $data = $this->getHistoricalData($zip_code, $range);
+        return '';
+    }
+
+    /**
+     * @todo - add functionality to read and filter data file
+     * 
+     * Get historical data based on zip code and range
+     * @param {string} $zip_code: zip code to get estimate for
+     * @param {array} $range: date range for which historical data should be used
+     * @return {array} array with historical data
+     *
+     */
+    private function getHistoricalData(string $zip_code, array $range): array
+    {
+        return [];
     }
 }
