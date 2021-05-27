cd6e3ac790bd4d3e63325352d46b6281520e7d4d
Razvan Goldstein <razvan.goldstein@gmail.com>
2021-05-27 11:16:01 +0300

    added test for custom date string format

--- tests/Shipping/UtilsTest.php
+++ tests/Shipping/UtilsTest.php
@@ -12,5 +12,8 @@ class UtilsTest extends TestCase
     public function testCreateDateString(): void
     {   
         $this->assertSame('1970-01-01 00:00:00', Utils::createDateString(0));
+
+        // 2021-05-05 10:00:00 UTC
+        $this->assertSame('2021-05-05', Utils::createDateString(1620208800, 'Y-m-d'));
     }
 }
