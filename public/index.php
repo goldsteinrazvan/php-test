e7d6998b0651b7978be5cd49a688224b9065c332
Razvan Goldstein <razvan.goldstein@gmail.com>
2021-05-28 12:57:19 +0300

    removed example for single value range

--- public/index.php
+++ public/index.php
@@ -25,9 +25,3 @@ echo "Estimated delivery (no range given): $estimated_delivery_no_range\n";
 $range = [1596240000, 1609459199];
 $estimated_delivery = $shipping->getEstimatedDelivery($zip_code, $range);
 echo "Estimated delivery (range august-december): $estimated_delivery\n";
-
-
-// just starting month
-$one_month_range = [];
-$estimated_delivery_one_month = $shipping->getEstimatedDelivery($zip_code, $range);
-echo "Estimated delivery (just starting month): $estimated_delivery\n";
